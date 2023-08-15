e6313c23f2e0e6a537a5029e1d91fd5d442d2a90
anthonycalcagno9 <acalcagno9@gmail.com>
2023-08-15 00:13:28 -0500

    lot of stuff

--- app/page.tsx
+++ app/page.tsx
@@ -38,7 +38,7 @@ export default function Home() {
           <h2 className="text-center text-slate-950 w-2/3">Attackers are meant to be used aggresively and distrupt your opponent by destroying their resources</h2>
         </div>
       </div>
-      <div className="flex flex-col items-center justify-center w-full py-20 items-start bg-gray">
+      <div className="flex flex-col items-center justify-center w-full py-20 items-start bg-gray-100">
         <h1 className="text-center font-bold text-5xl text-slate-950 pb-12">Connect with Us!</h1>
         <div>
           <button className="bg-[#1877F2] hover:bg-teal-400 text-white font-bold rounded w-32 h-12 px-6 py-2 mx-12 my-2">
@@ -55,7 +55,29 @@ export default function Home() {
           </button>
         </div>
       </div>
-      <div className="flex items-center justify-center w-full h-[65vh] sm:h-[65vh] bg-darker-gray">
+      <div className="flex flex-row items-center justify-center w-full h-[65vh] sm:h-[65vh] bg-gray-200">
+        <div className="flex flex-col w-[] px-20">
+          <h1 className="text-slate-950 w-50vw64 text-4xl mb-4">Thank you for visiting our website!</h1>
+          <h2 className="text-slate-950 w-56 text-2xl pb-20">We cant wait to send you our game!</h2>
+        </div>
+        <div className="flex flex-col items-center bg-sky-50 p-10 mx-20 rounded w-86 h-106">
+          <div className="flex flex-col items-left">
+            <label htmlFor="first_name" className="block mb-2 text-slate-950">First name*</label>
+            <input type="text" id="first_name" className="bg-gray-100 border border-gray-300 text-slate-950 text-sm rounded mb-2 focus:ring-blue-500 focus:border-blue-500 block w-56 h-8" required></input>
+          </div>
+          <div className="flex flex-col items-left">
+            <label htmlFor="first_name" className="block mb-2 text-slate-950">Last name*</label>
+            <input type="text" id="first_name" className="bg-gray-100 border border-gray-300 text-slate-950 text-sm rounded mb-2 focus:ring-blue-500 focus:border-blue-500 block w-56 h-8" required></input>
+          </div>
+          <div className="flex flex-col items-left">
+            <label htmlFor="first_name" className="block mb-2 text-slate-950">Email address*</label>
+            <input type="text" id="first_name" className="bg-gray-100 border border-gray-300 text-slate-950 text-sm rounded mb-2 focus:ring-blue-500 focus:border-blue-500 block w-56 h-8" required></input>
+          </div>
+          <button className="bg-orange-400 hover:bg-teal-400 text-white font-bold rounded my-4 w-56 h-10">
+             Sign Up
+          </button>
+          <p className="text-xs text-slate-950 text-center w-64 break-normal">Signing up will put you on our email list! We will send you updates on the games development</p>
+        </div>
       </div>
     </main>
     </ConfigProvider>
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,13 +10,7 @@ module.exports = {
       backgroundImage: {
         'hero-pattern': "linear-gradient(to_right_bottom,rgba(0, 122, 255, 0.8),rgba(16,71,52,0.8)),url('/The+Enchanted+Forest+Banner.jpeg')",
       },
-      colors: {
-        'primary-blue': '#007AFF',
-        'primary-light-blue': '#00BEFF',
-        'primary-yellow': '#FFED72',
-        'gray': '#DDDDDD',
-        'darker-gray': '#D4D4D4'
-      }
+      colors: {}
     }
   },
   plugins: [],
